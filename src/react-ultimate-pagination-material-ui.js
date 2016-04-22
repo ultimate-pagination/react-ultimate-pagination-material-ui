7bb281812fe42b6ab0a3f8564b91e1333b940edf
Dmytro Yarmak <dmytroy@backbase.com>
2016-04-22 09:15:39 +0300

    Make button squares

--- src/react-ultimate-pagination-material-ui.js
+++ src/react-ultimate-pagination-material-ui.js
@@ -6,28 +6,32 @@ import NavigationLastPage from 'material-ui/svg-icons/navigation/last-page';
 import NavigationChevronLeft from 'material-ui/svg-icons/navigation/chevron-left';
 import NavigationChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
 
+const flatButtonStyle = {
+  minWidth: 36
+};
+
 const Page = ({value, isActive, onClick}) => (
-  <FlatButton label={value.toString()} primary={isActive} onClick={onClick}/>
+  <FlatButton style={flatButtonStyle} label={value.toString()} primary={isActive} onClick={onClick}/>
 );
 
 const Ellipsis = () => (
-  <FlatButton label="..." disabled={true} />
+  <FlatButton style={flatButtonStyle} label="..." disabled={true} />
 );
 
 const FirstPageLink = ({isActive, onClick}) => (
-  <FlatButton icon={<NavigationFirstPage/>} disabled={isActive} onClick={onClick}/>
+  <FlatButton style={flatButtonStyle} icon={<NavigationFirstPage/>} disabled={isActive} onClick={onClick}/>
 );
 
 const PreviousPageLink = ({isActive, onClick}) => (
-  <FlatButton icon={<NavigationChevronLeft/>} disabled={isActive} onClick={onClick}/>
+  <FlatButton style={flatButtonStyle} icon={<NavigationChevronLeft/>} disabled={isActive} onClick={onClick}/>
 );
 
 const NextPageLink = ({isActive, onClick}) => (
-  <FlatButton icon={<NavigationChevronRight/>} disabled={isActive} onClick={onClick}/>
+  <FlatButton style={flatButtonStyle} icon={<NavigationChevronRight/>} disabled={isActive} onClick={onClick}/>
 );
 
 const LastPageLink = ({isActive, onClick}) => (
-  <FlatButton icon={<NavigationLastPage/>} disabled={isActive} onClick={onClick}/>
+  <FlatButton style={flatButtonStyle} icon={<NavigationLastPage/>} disabled={isActive} onClick={onClick}/>
 );
 
 const itemTypeToMaterialUiComponent = {
