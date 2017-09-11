eae242e72c99b8f157205166a44121acac979931
tony <anthony.kerz@gmail.com>
2017-09-10 22:33:23 -0400

    mui-next

--- src/react-ultimate-pagination-material-ui.js
+++ src/react-ultimate-pagination-material-ui.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {createUltimatePagination, ITEM_TYPES} from 'react-ultimate-pagination';
+import jss from 'jss';
+import classnames from 'classnames';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import NavigationFirstPage from 'material-ui-icons/FirstPage';
@@ -7,46 +9,62 @@ import NavigationLastPage from 'material-ui-icons/LastPage';
 import NavigationChevronLeft from 'material-ui-icons/ChevronLeft';
 import NavigationChevronRight from 'material-ui-icons/ChevronRight';
 
+const {classes} = jss
+  .createStyleSheet({
+    button: {
+      height: 36,
+      width: 36,
+      minWidth: 36
+    },
+    wrapper: {
+      display: 'flex'
+    }
+  })
+  .attach();
+
 export default function ({buttonClass, wrapperClass, wrapperComponent} = {}) {
+  const buttonClasses = classnames(classes.button, buttonClass)
+  const wrapperClasses = classnames(classes.wrapper, wrapperClass)
+
   const Page = ({value, isActive, onClick}) => (
-    <Button className={buttonClass} color={isActive ? 'primary' : 'default'} onClick={onClick}>
+    <Button className={buttonClasses} color={isActive ? 'primary' : 'default'} onClick={onClick}>
       {value.toString()}
     </Button>
   );
 
   const Ellipsis = ({onClick}) => (
-    <Button className={buttonClass} label="..." onClick={onClick}>
+    <Button className={buttonClasses} label="..." onClick={onClick}>
       ...
     </Button>
   );
 
   const FirstPageLink = ({isActive, onClick}) => (
-    <IconButton className={buttonClass} onClick={onClick}>
+    <IconButton className={buttonClasses} onClick={onClick}>
       <NavigationFirstPage/>
     </IconButton>
   );
 
   const PreviousPageLink = ({isActive, onClick}) => (
-    <IconButton className={buttonClass} onClick={onClick}>
+    <IconButton className={buttonClasses} onClick={onClick}>
       <NavigationChevronLeft/>
     </IconButton>
   );
 
   const NextPageLink = ({isActive, onClick}) => (
-    <IconButton className={buttonClass} onClick={onClick}>
+    <IconButton className={buttonClasses} onClick={onClick}>
       <NavigationChevronRight/>
     </IconButton>
   );
 
   const LastPageLink = ({isActive, onClick}) => (
-    <IconButton className={buttonClass} onClick={onClick}>
+    <IconButton className={buttonClasses} onClick={onClick}>
       <NavigationLastPage/>
     </IconButton>
   );
 
   function WrapperComponent(props) {
     // default style here (use {display: 'flex'} to horizontally align `Button` and `IconButton`)
-    return wrapperComponent || <div style={{display: 'flex'}} className={wrapperClass}>{props.children}</div>
+    return wrapperComponent || <div className={wrapperClasses}>{props.children}</div>
   };
 
   const itemTypeToComponent = {
